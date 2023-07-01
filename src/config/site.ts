43e95ed48faa35f57c5505535e8cf5a8a5e5b69e
Ricardo Castro <ricardocastro.dev@gmail.com>
2023-07-01 05:16:05 +0000

    change name

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig
 
 export const siteConfig = {
-  name: "Dulcesdlissa: Aprende Reposteria Online",
+  name: "Dulcesdlissa",
   description:
     "An open source e-commerce skateshop build wDulcesdlissa es una plataforma online de cursos de repostería ubicada en Tocuyito, Venezuela. Aprende a preparar una amplia variedad de postres, desde deliciosas tortas hasta pasteles, con nuestros cursos 100% prácticos. Nuestros expertos instructores te guiarán en todo momento para que puedas crear tus delicias dulces desde casa. Conviértete en un chef repostero y satisface los gustos dulces del día a día. ¡Regístrate ahora en Dulcesdlissa y comienza a crear tus postres más deliciosos!",
   url: "https://dulcesdlissa.com/",
